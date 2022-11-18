75938ae040d5b9a6fc56c0a7705341737f9bc3f9
topias <topias@mariapori.fi>
2022-11-18 13:39:30 +0200

    Threadingia

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from datetime import datetime
+from threading import Thread
 from time import time
 import cv2
 import discord
 video = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 timeLock = time()
+
+def WebhookSend(faceFrame):
+    filename = f"tunkeilija-{datetime.now().strftime('%d-%m-%Y-%H-%M-%S')}.png"
+    cv2.imwrite(filename, faceFrame)
+    webhook = discord.SyncWebhook.from_url("https://discord.com/api/webhooks/1042896663901122670/eRAmNF0XP25s4DNcE1wsVina8Cwm4m_t6thpsy5u4V7egT7UjitW3rh5Og0aC29v0Jd8")
+    embed = discord.Embed(title="Tunkeilija!", description="Kasvot havaittu! :warning:")
+    file = discord.File(filename, filename="image.png")
+    embed.set_image(url="attachment://image.png")
+    webhook.send(file=file, embed=embed)
 while True:
     _, frame = video.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -12,14 +22,10 @@ while True:
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
         if time() >= timeLock:
-            timeLock = time() + 60
-            filename = f"tunkeilija-{datetime.now().strftime('%d-%m-%Y-%H-%M-%S')}.png"
-            cv2.imwrite(filename, frame)
-            webhook = discord.SyncWebhook.from_url("your_webhook_url")
-            embed = discord.Embed(title="Tunkeilija!", description="Kasvot havaittu! :warning:")
-            file = discord.File(filename, filename="image.png")
-            embed.set_image(url="attachment://image.png")
-            webhook.send(file=file,embed=embed)
+            timeLock = time() + 30
+            job = Thread(target=WebhookSend(frame))
+            job.start()
+            break
 
     cv2.imshow("Mariaporin turvakamera", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
